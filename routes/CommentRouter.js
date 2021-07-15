3b0440cb45157883037e0f50aaf75945063dc58e
Alexander Zerra <Alexander.Zerra@gmail.com>
2021-07-15 14:20:17 -0700

    fixed comments to all CRUD is operational

--- routes/CommentRouter.js
+++ routes/CommentRouter.js
@@ -6,6 +6,6 @@ Router.get('/', controller.GetAllComments)
 Router.get('/user/:user_id', controller.GetCommentByUserId)
 Router.get('/gym/:gym_id', controller.GetCommentByGymId)
 Router.put('/:comment_id', controller.UpdateComment)
-Router.delete('/"comment_id', controller.DeleteComment)
+Router.delete('/:comment_id', controller.DeleteComment)
 
 module.exports = Router
