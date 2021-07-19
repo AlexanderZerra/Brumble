fd49b23ac562e228d5394ffcc3ffc8a1e417076b
Alexander Zerra <Alexander.Zerra@gmail.com>
2021-07-18 19:18:20 -0700

    Comment Card work on handleDelete

--- client/src/components/CommentCard.js
+++ client/src/components/CommentCard.js
@@ -4,12 +4,21 @@ import { BASE_URL } from '../globals'
 
 const CommentCard = (props) => {
   console.log(props.comment.post)
+
+  // const handleSubmit = async (e) => {
+  //   e.preventDefault()
+  //   await axios.put (`${}/comment/${}`,{
+  //     userId: props.user_id,
+  //     gymId: props.gym_id,
+  //     post: props.post
+  //   })
+  // }
   return (
     <div>
-      <p>{props.comment.post}</p>
-      {/* <form onSubmit={handleSubmit}></form>
+      {/* <p>{props.comment.post}</p>
+      <form onSubmit={handleSubmit}></form>
       <button>Submit</button>
-      
+
       <button onClick={() => props.handleDelete(props.id)}>Delete</button> */}
     </div>
   )
