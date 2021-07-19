0a3bd2fd6dd9827ccecd8fbf8bb96f217ffefd85
Alexander Zerra <Alexander.Zerra@gmail.com>
2021-07-19 07:51:51 -0700

    clean up comment card

--- client/src/components/CommentCard.js
+++ client/src/components/CommentCard.js
@@ -1,21 +1,6 @@
-import { React, useState } from 'react'
-import axios from 'axios'
-import { BASE_URL } from '../globals'
+import React from 'react'
 
 const CommentCard = (props) => {
-  console.log(props.comment)
-
-  // const handleDelete = async (e) => {
-  //   try {
-  //     await axios.delete(`${BASE_URL}/comment/${props.comment.id}`)
-  //     props.setComments(
-  //       props.comments.filter((comment) => comment.id !== props.comment.id)
-  //     )
-  //   } catch (error) {
-  //     throw error
-  //   }
-  // }
-
   return (
     <div>
       <p>{props.comment.post}</p>
