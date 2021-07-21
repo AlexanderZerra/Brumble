b0b7cb3c8e6e56ad675bcde9a4b8851e5bf38ebc
Alexander Zerra <Alexander.Zerra@gmail.com>
2021-07-20 18:56:38 -0700

    Styling of Home Page

--- client/src/pages/HomePage.js
+++ client/src/pages/HomePage.js
@@ -1,26 +1,28 @@
 import React from 'react'
 const HomePage = (props) => {
   return (
-    <body className="homebody">
+    <div className="homebody">
       <div className="body">
         <div className="home">
-          <h1 className="header">Welcome to Brumble.</h1>
-          <p className="mission">
-            Welcome to the Brumble App, this app is brought to you to help like
-            minded individuals find others to connect and workout with. In
-            todays fast pace society, going to the gym with a friend isnt always
-            the option due to schedules matching up. Here at Brumble we took the
-            idea that no one new or seasoned should have to worry about not
-            gettin the GAINZ. Have you thought to yourself...I want to hit a new
-            PR but my buddy isnt here today to spot me...but i dont know anyone
-            ?...Thats where Brumble comes into play. With verified testimony of
-            other like minded individuals such as yourself. We have created a
-            database that allows you to find someone to help you out in your
-            area.
-          </p>
+          <div className="homediv" style={{ paddingTop: '100px' }}>
+            <h1 className="header">Welcome to Brumble.</h1>
+            <p className="mission">
+              Welcome to the Brumble App, this app is brought to you to help
+              like minded individuals find others to connect and workout with.
+              In todays fast pace society, going to the gym with a friend isnt
+              always the option due to schedules matching up. Here at Brumble we
+              took the idea that no one new or seasoned should have to worry
+              about not gettin the GAINZ. Have you thought to yourself...I want
+              to hit a new PR but my buddy isnt here today to spot me...but i
+              dont know anyone ?...Thats where Brumble comes into play. With
+              verified testimony of other like minded individuals such as
+              yourself. We have created a database that allows you to find
+              someone to help you out in your area.
+            </p>
+          </div>
         </div>
       </div>
-    </body>
+    </div>
   )
 }
 export default HomePage
