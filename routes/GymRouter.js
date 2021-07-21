e9dbe5f622a084236ee6a39d4f91e759898419f3
Alexander Zerra <Alexander.Zerra@gmail.com>
2021-07-20 18:41:28 -0700

    clean up Routers

--- routes/GymRouter.js
+++ routes/GymRouter.js
@@ -5,5 +5,5 @@ Router.put('/:gym_id', controller.UpdateGym)
 Router.post('/', controller.CreateGym)
 Router.get('/', controller.GetAllGyms)
 Router.get('/:gym_id', controller.GetGymById)
-// Router.get('/:user_id', controller.GetGymByUserId)
+
 module.exports = Router
