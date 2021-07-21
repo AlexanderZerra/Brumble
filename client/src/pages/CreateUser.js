c73460b8835cbc8b5eb69e719fafb442b6d77af2
Alexander Zerra <Alexander.Zerra@gmail.com>
2021-07-20 18:32:58 -0700

    CreateUser clean up

--- client/src/pages/CreateUser.js
+++ client/src/pages/CreateUser.js
@@ -4,8 +4,6 @@ import { useState } from 'react'
 import axios from 'axios'
 import { BASE_URL } from '../globals'
 
-//user id is your code to add gyms
-
 const CreateUser = (props) => {
   const [name, setName] = useState('')
   const [location, setLocation] = useState('')
