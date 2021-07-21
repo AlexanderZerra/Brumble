3a186c3da31c17c7eed0cbd42a7c288142b241aa
Alexander Zerra <Alexander.Zerra@gmail.com>
2021-07-20 20:08:13 -0700

    added serverjs

--- server.js
+++ server.js
@@ -19,4 +19,11 @@ app.get('*', (req, res) =>
   res.sendFile(path.join(__dirname, 'client', 'build', 'index.html'))
 )
 
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname, 'client/build')))
+  app.get('*', (req, res) => {
+    res.sendFile(path.join(`${__dirname}/client/build/index.html`))
+  })
+}
+
 app.listen(PORT, () => console.log(`Server Started On Port: ${PORT}`))
